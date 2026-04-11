a01b53dbfa4307f1beb52f87bf03a082593a8d90
Jhon monsalve <99751739+Assasin1116@users.noreply.github.com>
2026-04-11 15:36:34 -0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import HildaPage from './pages/HildaPage'
-import SantiagoPage from './pages/SantiagoPage'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import MiguelPage from "./pages/Miguelpage";
 
 function App() {
   return (
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
-        <Route path="/" element={<Navigate to="/hilda-palacios" replace />} />
-        <Route path="/hilda-palacios" element={<HildaPage />} />
-        <Route path="/santiago-castaneda" element={<SantiagoPage />} />
+        <Route path="/" element={<Navigate to="/miguel-hernandez" replace />} />
+        <Route path="/miguel-hernandez" element={<MiguelPage />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
