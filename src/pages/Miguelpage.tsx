ac5df6648dbc6b941bd14a70c1a3b03c4c75a9ef
Jhon monsalve <99751739+Assasin1116@users.noreply.github.com>
2026-04-15 11:04:41 -0500

    comente

--- src/pages/Miguelpage.tsx
+++ src/pages/Miguelpage.tsx
@@ -1,3 +1,4 @@
+/*
 import { useEffect } from "react";
 import HeroSection from "../components/hilda/HeroSection";
 import AddContactModal from "../components/AddContactModal";
@@ -5,26 +6,27 @@ import NortechSection from "../components/miguel/NortechSection";
 import ContactSection from "../components/miguel/ContactSection";
 
 export default function MiguelPage() {
-  useEffect(() => {
-    document.title = "Miguel Hernandez";
-  }, []);
+useEffect(() => {
+  document.title = "Miguel Hernandez";
+}, []);
 
-  return (
-    <div className="overflow-x-hidden">
-      <AddContactModal
-        contact={{
-          name: "Miguel Hernandez",
-          phone: "+57 313 8488257",
-        }}
-        storageKey="hasSeenContactModal_Miguel"
-      />
-      <div className="bg-white">
-        <div className="bg-gradient-to-br from-[#9b6cf8] to-[#b89dfa] rounded-b-[80px] md:rounded-b-[6%]">
-          <HeroSection />
-        </div>
+return (
+<div className="overflow-x-hidden">
+    <AddContactModal
+      contact={{
+        name: "Miguel Hernandez",
+        phone: "+57 313 8488257",
+      }}
+      storageKey="hasSeenContactModal_Miguel"
+    />
+    <div className="bg-white">
+      <div className="bg-gradient-to-br from-[#9b6cf8] to-[#b89dfa] rounded-b-[80px] md:rounded-b-[6%]">
+        <HeroSection />
       </div>
-      <NortechSection />
-      <ContactSection />
     </div>
-  );
+    <NortechSection />
+    <ContactSection />
+  </div>
+);
 }
+*/
